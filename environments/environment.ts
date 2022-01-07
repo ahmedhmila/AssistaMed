1b71f739703ac849b32165a1cd98a14c5ef15bfd
Ahmed Hmila <74432440+ahmedhmila@users.noreply.github.com>
2022-01-07 09:36:05 +0100

    Update environment.ts

--- environments/environment.ts
+++ environments/environment.ts
@@ -2,12 +2,6 @@ export const environment = {
   production: false,
   firebaseConfig: {
    
-      apiKey: "AIzaSyDEmv45FjKuiGlVv302HR3kD-cniyIFj9A",
-      authDomain: "angularfinalsaoudi.firebaseapp.com",
-      projectId: "angularfinalsaoudi",
-      storageBucket: "angularfinalsaoudi.appspot.com",
-      messagingSenderId: "661479499369",
-      appId: "1:661479499369:web:6691ce70cc0ed47131e4f8",
-      measurementId: "G-VNDY6Z8W08"
+      xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
   }
 };
